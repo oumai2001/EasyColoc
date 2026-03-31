92746ec336bc4eb1363f123f501a5bcbbd73789f
oumai2001 <oumaimaeddahani0@gmail.com>
2026-03-31 01:07:30 +0000

    Fix Controller.php conflict

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <nav x-data="{ open: false }" class="bg-gradient-to-r from-blue-600 via-indigo-600 to-purple-600 shadow-lg">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -35,28 +34,6 @@
                             </svg>
                             <span class="text-[#000000]">Mes Colocations</span>
                         </div>
-=======
-<nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
-    <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
-            <div class="flex">
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
-                    </a>
-                </div>
-
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                    
-                    <x-nav-link :href="route('colocations.index')" :active="request()->routeIs('colocations.*')">
-                        {{ __('Mes Colocations') }}
->>>>>>> 8e94925080aafa664147f8b1fd6ee70babb48e2c
                     </x-nav-link>
                 </div>
             </div>
